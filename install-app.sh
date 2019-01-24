58fef692e2436d86024c3fb148f21800bbb379a1
hpeters83 <hpeters@tibco.com>
2019-01-24 09:56:15 +0100

    app

---
+++ install-app.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo Install App Files...
+export npm_loc='/usr/local/lib/node_modules/@angular/cli/node_modules/'
+rm -rf ${npm_loc}/@custom
+mkdir -p ${npm_loc}/@custom/mycloudapp
+cp -R ./src/cloud-app/files/* ${npm_loc}/@custom/mycloudapp/
+
+
+
